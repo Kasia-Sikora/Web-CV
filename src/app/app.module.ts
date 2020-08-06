a7956e79ebde1739f60c8739ef78017084d0f955
Kasia-Sikora <sikora.katarzyna.waw@gmail.com>
2020-08-06 10:46:09 +0200

    Refactor all and add personal content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { PersonalComponent } from './personal/personal.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    PersonalComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
